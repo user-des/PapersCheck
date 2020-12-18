2b06c8fd953273fe29b2d5915221270f8e655c66
Desi <74535637+user-des@users.noreply.github.com>
2020-12-19 00:36:31 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,6 +12,15 @@ btnclose.addEventListener('click', btnClickeClose);
 function btnClickeClose(){
     conte.classList.add("contant-hidden");
 }
+
+
+document.addEventListener('click', function(){
+    console.log("hi");
+    if(!conte.classList.contains('contant-hidden')){
+        console.log("dddd")
+    }
+})
+
 // ффффффффффффффффффффффффффффффффффффффффффф
 // const btnMenu = document.querySelector('.wrapper-ico');
 // const menu = document.querySelector('.setting-nav');
@@ -29,3 +38,45 @@ function btnClickeClose(){
 //         conte.classList.toggle('contant-hidden');
 //     }
 // });
+
+
+
+
+
+//логика для мини бургера карточки
+//открытие и закрытие бургера
+let miburbtn = document.querySelector('.miniburger');
+let minibur = document.querySelector('#minibur');
+let cardAvtoe = document.querySelector('#card_avtot');
+
+miburbtn.addEventListener('click', miniBurger);
+
+function miniBurger(){
+    minibur.classList.toggle("display_none");
+    cardAvtoe.classList.toggle("display_none");
+}
+//открытие и закрытие соц сетей
+let openMenuShareBtn = document.querySelector('.open-menu-share');
+let menuShare = document.querySelector('#menu-share');
+
+openMenuShareBtn.addEventListener('click', menuShareOpen);
+
+function menuShareOpen(){
+    menuShare.classList.toggle("display_none");
+}
+
+//добавление и удаление карточки
+let addCardBtn = document.querySelector('.add-card');
+let delCard = document.querySelector('#delcard');
+
+addCardBtn.addEventListener('click', addCard);
+function addCard(){
+    delCard.classList.toggle("display_none");
+    addCardBtn.classList.toggle("display_none");
+}
+
+delCard.addEventListener('click', DelCard);
+function DelCard(){
+    delCard.classList.toggle("display_none");
+    addCardBtn.classList.toggle("display_none");
+}
